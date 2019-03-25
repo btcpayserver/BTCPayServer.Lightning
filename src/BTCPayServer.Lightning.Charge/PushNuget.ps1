ee169364bbdae623d0736ba711befe3e74a625a0
nicolas.dorier <nicolas.dorier@gmail.com>
2019-03-25 11:33:51 +0900

    Create snupkg

--- src/BTCPayServer.Lightning.Charge/PushNuget.ps1
+++ src/BTCPayServer.Lightning.Charge/PushNuget.ps1
@@ -1,6 +1,7 @@
 Remove-Item "bin\release\" -Recurse -Force
-dotnet pack --configuration Release
-dotnet nuget push "bin\Release\*.nupkg" --source "https://api.nuget.org/v3/index.json"
+dotnet pack --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg
+$package=(ls .\bin\Release\*.nupkg).FullName
+dotnet nuget push $package --source "https://api.nuget.org/v3/index.json"
 $ver = ((Get-ChildItem .\bin\release\*.nupkg)[0].Name -replace '[^\d]*\.(\d+(\.\d+){1,4}).*', '$1')
 git tag -a "Charge/v$ver" -m "Charge/$ver"
 git push --tags
