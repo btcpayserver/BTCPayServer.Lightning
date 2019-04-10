ea34ef4601ed810992bf882fb14d493dad61a7c7
nicolas.dorier <nicolas.dorier@gmail.com>
2019-04-10 18:35:22 +0900

    help script to push nuget all packages

---
+++ src/PushNuget.ps1
@@ -0,0 +1,19 @@
+cd BTCPayServer.Lightning.Common
+PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.CLightning
+PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.Charge
+PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.LND
+PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.All
+PushNuget.ps1
+cd ..
