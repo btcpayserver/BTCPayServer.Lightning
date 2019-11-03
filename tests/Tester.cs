53abd5c67bf66b877a1253da00deebc0008b439e
nicolas.dorier <nicolas.dorier@gmail.com>
2019-11-03 15:16:20 +0900

    Remove eclair from tests

--- tests/Tester.cs
+++ tests/Tester.cs
@@ -83,24 +83,24 @@ namespace BTCPayServer.Lightning.Tests
 
         public static IEnumerable<(string Name, ILightningClient Client)> GetLightningClients()
         {
-            //yield return ("Charge (Client)", CreateChargeClient());
-            //yield return ("C-Lightning (Client)", CreateCLightningClient());
-            //yield return ("LND (Client)", CreateLndClient());
-            yield return ("Eclair (Client)", CreateEclairClient());
+            yield return ("Charge (Client)", CreateChargeClient());
+            yield return ("C-Lightning (Client)", CreateCLightningClient());
+            yield return ("LND (Client)", CreateLndClient());
+            // yield return ("Eclair (Client)", CreateEclairClient());
         }
 
         public static IEnumerable<(string Name, ILightningClient Client)> GetLightningSenderClients()
         {
-            //yield return ("C-Lightning (Client)", CreateCLightningClient());
-            //yield return ("LND (Client)", CreateLndClient());
-            yield return ("Eclair (Client)", CreateEclairClient());
+            yield return ("C-Lightning (Client)", CreateCLightningClient());
+            yield return ("LND (Client)", CreateLndClient());
+            // yield return ("Eclair (Client)", CreateEclairClient());
         }
 
         public static IEnumerable<(string Name, ILightningClient Client)> GetLightningDestClients()
         {
-            //yield return ("C-Lightning (Client)", CreateCLightningClientDest());
-            //yield return ("LND (Client)", CreateLndClientDest());
-            yield return ("Eclair (Client)", CreateEclairClientDest());
+            yield return ("C-Lightning (Client)", CreateCLightningClientDest());
+            yield return ("LND (Client)", CreateLndClientDest());
+            // yield return ("Eclair (Client)", CreateEclairClientDest());
         }
     }
 }
