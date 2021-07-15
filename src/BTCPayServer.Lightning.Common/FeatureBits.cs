4bfbb16d76417b2a8e479aa3753b4d1e930dd2e1
nicolas.dorier <nicolas.dorier@gmail.com>
2021-07-15 22:44:06 +0900

    Transform comments into <summary>

--- src/BTCPayServer.Lightning.Common/FeatureBits.cs
+++ src/BTCPayServer.Lightning.Common/FeatureBits.cs
@@ -7,33 +7,45 @@ namespace BTCPayServer.Lightning
     {
         None = 0,
 
-        // TLVOnionPayloadRequired is a feature bit that indicates a node is
-        // able to decode the new TLV information included in the onion packet.
+        /// <summary>
+        /// TLVOnionPayloadRequired is a feature bit that indicates a node is
+        /// able to decode the new TLV information included in the onion packet.
+        /// </summary>
         TLVOnionPayloadRequired = 1 << 8,
 
-        // TLVOnionPayloadOptional is an optional feature bit that indicates a
-        // node is able to decode the new TLV information included in the onion
-        // packet.
+        /// <summary>
+        /// TLVOnionPayloadOptional is an optional feature bit that indicates a
+        /// node is able to decode the new TLV information included in the onion
+        /// packet.
+        /// </summary>
         TLVOnionPayloadOptional = 1 << 9,
 
-        // PaymentAddrRequired is a required feature bit that signals that a
-        // node requires payment addresses, which are used to mitigate probing
-        // attacks on the receiver of a payment.
+        /// <summary>
+        /// PaymentAddrRequired is a required feature bit that signals that a
+        /// node requires payment addresses, which are used to mitigate probing
+        /// attacks on the receiver of a payment.
+        /// </summary>
         PaymentAddrRequired = 1 << 14,
 
-        // PaymentAddrOptional is an optional feature bit that signals that a
-        // node supports payment addresses, which are used to mitigate probing
-        // attacks on the receiver of a payment.
+        /// <summary>
+        /// PaymentAddrOptional is an optional feature bit that signals that a
+        /// node supports payment addresses, which are used to mitigate probing
+        /// attacks on the receiver of a payment.
+        /// </summary>
         PaymentAddrOptional = 1 << 15,
 
-        // MPPOptional is a required feature bit that signals that the receiver
-        // of a payment requires settlement of an invoice with more than one
-        // HTLC.
+        /// <summary>
+        /// MPPOptional is a required feature bit that signals that the receiver
+        /// of a payment requires settlement of an invoice with more than one
+        /// HTLC.
+        /// </summary>
         MPPRequired = 1 << 16,
 
-        // MPPOptional is an optional feature bit that signals that the receiver
-        // of a payment supports settlement of an invoice with more than one
-        // HTLC.
+        /// <summary>
+        /// MPPOptional is an optional feature bit that signals that the receiver
+        /// of a payment supports settlement of an invoice with more than one
+        /// HTLC.
+        /// </summary>
         MPPOptional = 1 << 17
     }
 }
