2c40b1f81b6ccb5cdfaabfc1d2b91cf63bd9bfd6
Dennis Reimann <mail@dennisreimann.de>
2022-09-23 18:17:36 +0200

    Format files

--- src/BTCPayServer.Lightning.Common/LightningInvoice.cs
+++ src/BTCPayServer.Lightning.Common/LightningInvoice.cs
@@ -1,17 +1,16 @@
 using System;
 using System.Collections.Generic;
 
-namespace BTCPayServer.Lightning
+namespace BTCPayServer.Lightning;
+
+public class LightningInvoice
 {
-    public class LightningInvoice
-    {
-        public string Id { get; set; }
-        public LightningInvoiceStatus Status { get; set; }
-        public string BOLT11 { get; set; }
-        public DateTimeOffset? PaidAt { get; set; }
-        public DateTimeOffset ExpiresAt { get; set; }
-        public LightMoney Amount { get; set; }
-        public LightMoney AmountReceived { get; set; }
-        public Dictionary<ulong, string> CustomRecords { get; set; }
-    }
+    public string Id { get; set; }
+    public LightningInvoiceStatus Status { get; set; }
+    public string BOLT11 { get; set; }
+    public DateTimeOffset? PaidAt { get; set; }
+    public DateTimeOffset ExpiresAt { get; set; }
+    public LightMoney Amount { get; set; }
+    public LightMoney AmountReceived { get; set; }
+    public Dictionary<ulong, string> CustomRecords { get; set; }
 }
--- src/BTCPayServer.Lightning.Common/PayInvoiceParams.cs
+++ src/BTCPayServer.Lightning.Common/PayInvoiceParams.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using NBitcoin;
 
-namespace BTCPayServer.Lightning
+namespace BTCPayServer.Lightning;
+
+public class PayInvoiceParams
 {
-    public class PayInvoiceParams
-    {
-        public double? MaxFeePercent { get; set; }
-        public Money? MaxFeeFlat { get; set; }
-        public LightMoney? Amount { get; set; }
-        
-        public PubKey? Destination { get; set; }
-        
-        public uint256? PaymentHash { get; set; }
-        
-        public Dictionary<ulong,string>? CustomRecords { get; set; }
-    }
+    public double? MaxFeePercent { get; set; }
+    public Money? MaxFeeFlat { get; set; }
+    public LightMoney? Amount { get; set; }
+
+    public PubKey? Destination { get; set; }
+
+    public uint256? PaymentHash { get; set; }
+
+    public Dictionary<ulong, string>? CustomRecords { get; set; }
 }
