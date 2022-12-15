0450a9207b999fa9f4eb52e5eb209dbf7b5b1e61
d11n <mail@dennisreimann.de>
2022-12-15 07:25:17 +0100

    Eclair updates (#112)
    Extracted these from #111:
    
    - Incorporate payment timeout pattern from #106
    - Improve failure response
    - Fix missing payment info (some [`getsentinfo` properties](https://acinq.github.io/eclair/#getsentinfo) weren't correct)
    - Upgrade containers to 0.8.0

--- src/BTCPayServer.Lightning.Eclair/Models/SendToNodeRequest.cs
+++ src/BTCPayServer.Lightning.Eclair/Models/SendToNodeRequest.cs
@@ -4,7 +4,6 @@ namespace BTCPayServer.Lightning.Eclair.Models
     {
         public string NodeId { get; set; }
         public long? AmountMsat { get; set; }
-        public string PaymentHash { get; set; }
         public int? MaxAttempts { get; set; }
         public int? MaxFeePct { get; set; }
         public long? MaxFeeFlatSat { get; set; }
