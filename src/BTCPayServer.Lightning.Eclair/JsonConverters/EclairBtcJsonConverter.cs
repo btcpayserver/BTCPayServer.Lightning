3671f87174aa73ccc6a12f5f856dc152921a2683
Dennis Reimann <mail@dennisreimann.de>
2022-12-21 17:03:01 +0100

    LightMoney: Fix float conversion
    Eclair denominates global balance amounts in BTC and Charge seems to export large amounts as floats.
    
    The solution is to separate the two clearly and fix the float conversion in the `LightMoneyJsonConverter`.
    
    Fixes btcpayserver/btcpayserver#4383.

--- src/BTCPayServer.Lightning.Eclair/JsonConverters/EclairBtcJsonConverter.cs
+++ src/BTCPayServer.Lightning.Eclair/JsonConverters/EclairBtcJsonConverter.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Reflection;
+using System.Text;
+using NBitcoin;
 using NBitcoin.JsonConverters;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace BTCPayServer.Lightning.JsonConverters
+namespace BTCPayServer.Lightning.Eclair.JsonConverters
 {
-    public class LightMoneyJsonConverter : JsonConverter
+    public class EclairBtcJsonConverter : JsonConverter
     {
         public override bool CanConvert(Type objectType)
         {
@@ -23,12 +26,13 @@ namespace BTCPayServer.Lightning.JsonConverters
                 {
                     JsonToken.Null => null,
                     JsonToken.Integer => _longType.IsAssignableFrom(reader.ValueType)
-                        ? new LightMoney((long)reader.Value)
-                        : new LightMoney(long.MaxValue),
-                    JsonToken.Float => new LightMoney(Convert.ToInt64(reader.Value)),
+                        ? new LightMoney((long)reader.Value, LightMoneyUnit.BTC)
+                        : new LightMoney(long.MaxValue, LightMoneyUnit.BTC),
+                    // Eclair denominates global balance amounts in BTC, see https://acinq.github.io/eclair/#globalbalance
+                    JsonToken.Float => new LightMoney(Convert.ToDecimal(reader.Value), LightMoneyUnit.BTC),
                     JsonToken.String =>
                         // some of the c-lightning values have a trailing "msat" that we need to remove before parsing
-                        new LightMoney(long.Parse(((string)reader.Value).Replace("msat", ""), CultureInfo.InvariantCulture)),
+                        new LightMoney(long.Parse(((string)reader.Value).Replace("msat", ""), CultureInfo.InvariantCulture), LightMoneyUnit.BTC),
                     // Fix for Eclair having empty objects for zero amount cases, see https://acinq.github.io/eclair/#globalbalance
                     JsonToken.StartObject => JObject.Load(reader) != null ? LightMoney.Zero : null,
                     _ => null
@@ -36,13 +40,14 @@ namespace BTCPayServer.Lightning.JsonConverters
             }
             catch (InvalidCastException)
             {
-                throw new JsonObjectException("Money amount should be in millisatoshi", reader);
+                throw new JsonObjectException("Money amount should be in BTC", reader);
             }
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            writer.WriteValue(((LightMoney)value).MilliSatoshi);
+            writer.WriteValue(((LightMoney)value).ToUnit(LightMoneyUnit.Bit));
         }
     }
 }
+
