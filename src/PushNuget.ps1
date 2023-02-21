e9aa10a607d41f25093c6b7d0d447e11cbbb6ffd
Nicolas Dorier <nicolas.dorier@gmail.com>
2023-02-21 14:29:36 +0900

    Revert github actions for nuget package push and tag (#123)

---
+++ src/PushNuget.ps1
@@ -0,0 +1,31 @@
+cd BTCPayServer.Lightning.Common
+.\PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.CLightning
+.\PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.Charge
+.\PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.LNDhub
+.\PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.LND
+.\PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.Eclair
+.\PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.LNbank
+.\PushNuget.ps1
+cd ..
+
+cd BTCPayServer.Lightning.All
+.\PushNuget.ps1
+cd ..
