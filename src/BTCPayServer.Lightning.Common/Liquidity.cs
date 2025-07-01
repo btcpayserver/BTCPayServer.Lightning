ae951d79d53c2587ef59dfdc450b21c7df6a1857
perls <info@megalithic.me>
2025-06-30 18:57:00 -0700

    add liquidity.cs

---
+++ src/BTCPayServer.Lightning.Common/Liquidity.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BTCPayServer.Lightning
+{
+    public class Liquidity
+    {
+        public static string HelloWorld()
+        {
+            return "Hello World from BTCPayServer.Lightning.Common.Liquidity! - UPDATED";
+        }
+    }
+}
